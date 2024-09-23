c94126ccc776678b0458672be31a038dd9284bbd
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2024-09-23 13:55:56 +0100

    Update fileio.rs
    Fix fileio file size limit.

--- src/hosts/fileio/fileio.rs
+++ src/hosts/fileio/fileio.rs
@@ -8,7 +8,7 @@ use crate::hosts::utils::check_file_size_limit;
 
 const UPLOAD_URL: &str = "https://file.io";
 pub fn run(c: &mut UploaderClient, file_path: &PathBuf, file_size: usize, _cfg: &HashMap<String, HashMap<String, String>>, _s: &mut HashMap<String, HashMap<String, String>>) -> Result<String, Box<dyn Error>>{
-    check_file_size_limit(file_size, Size::MB200)?;
+    check_file_size_limit(file_size, Size::GB2)?;
 
     let resp = c.multipart_upload(UPLOAD_URL, "file".to_string(), file_path, None, None, None)?;
     let json_obj: UploadResp = resp.json()?;
